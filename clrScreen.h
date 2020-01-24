27e2ebdd79840c21cf5e7fe6dc5e82e3a512c30b
fvdb <frank@am-knie.de>
2020-01-24 10:55:08 +0100

    add Apple support

--- clrScreen.h
+++ clrScreen.h
@@ -8,6 +8,9 @@
 #elif WIN32 || _MSC_VER
 #include <cstdlib>
 #define clrScreen() system("cls")
+#elif __APPLE__
+#include "stdio.h"
+#define clrScreen() printf("\x1B[2J")
 #endif
 
 #endif // CLR_SCREEN_H
